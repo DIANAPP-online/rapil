3d9f3db73aab7837f59526680950ec9074297e0a
Woadex <a.kakunin23@gmail.com>
2025-03-09 14:21:47 +0800

    update exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,7 @@
 export {Authenticator} from "./authenticator"
 export {RequestBuilder} from "./requestBuilder"
 export {Resource} from "./resource"
-export {Field,AccessTokenType, BaseSchemaType, FilledObject, PullMethods, PushMethods, NeedReAuth} from "./resourceTypes"
+export {ResourceLoader} from './loader'
+export {ResourceStorage} from './storage'
+export {ResourceSession} from './session'
+export {Field,AccessTokenType, BaseSchemaType, FilledObject, PullMethods, PushMethods, NeedReAuth} from "./types"
