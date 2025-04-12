b7a414a26d5323f4110f35f624f4b839fb0dbe9a
Woadex <a.kakunin23@gmail.com>
2025-04-13 05:45:47 +0800

    update imports

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-export { type Authenticator } from './lib/authenticator'
+export { type Authenticator, OAuth2 } from './lib/authenticator'
+export { ResourceJSONLoader } from './lib/jsonLoader'
+export { ResourceJSONManipulate } from './lib/jsonManipulate'
+export { ResourcePhotoLoader } from './lib/photoLoader'
 export { RequestBuilder } from './lib/requestBuilder'
 export { Resource } from './lib/resource'
 export { ResourceSession } from './lib/session'
